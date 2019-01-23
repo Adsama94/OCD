4592d456ab473055fe31377deb6c7b5c77747dc0
Abhishek <abhishektrivedi16aug@gmail.com>
2019-01-23 23:40:53 +0530

    added UI Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SliderComponent } from './slider/slider.component';
+import { AboutComponent } from './about/about.component';
+import { WorkComponent } from './work/work.component';
+import { TeamComponent } from './team/team.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SliderComponent,
+    AboutComponent,
+    WorkComponent,
+    TeamComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
