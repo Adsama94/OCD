af2acdbfc6f13b13e2aaf183553ab31a51265000
Abhishek <abhishektrivedi16aug@gmail.com>
2019-01-28 00:52:23 +0530

    completed about section and started work section

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SliderComponent implements OnInit {
 
-  images = [1, 2, 3].map(() => `https://picsum.photos/1280/500?random&t=${Math.random()}`);
+  images = [1, 2, 3].map(() => `https://picsum.photos/1280/400?random&t=${Math.random()}`);
 //images = ['assets/pictures/img1.jpg','assets/pictures/img2.jpg','assets/pictures/img3.jpg'];
   constructor() { }
 
